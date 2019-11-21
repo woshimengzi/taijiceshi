ce1565596967fc6b0d5e379b7bc38e1fc577be4b
mengqingshuai <851115461@qq.com>
2019-11-21 15:45:24 +0800

    提交demo1

---
+++ demo1/src/main/java/com/taiji/demo1/demo1/controller/DemoController.java
@@ -0,0 +1,15 @@
+package com.taiji.demo1.demo1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class DemoController {
+    @GetMapping("/demo")
+    @ResponseBody
+    public  String getDemo(){
+        System.out.println("DemoController.getDemo");
+        return  "hello demo";
+    }
+}
