8c45af9a01634655cb6de5bf19a418da3510add3
mengqingshuai <851115461@qq.com>
2019-11-21 15:55:58 +0800

    测试提交

--- demo1/src/main/java/com/taiji/demo1/demo1/controller/DemoController.java
+++ demo1/src/main/java/com/taiji/demo1/demo1/controller/DemoController.java
@@ -10,6 +10,6 @@ public class DemoController {
     @ResponseBody
     public  String getDemo(){
         System.out.println("DemoController.getDemo");
-        return  "hello demo";
+        return  "hello demo fdsfds";
     }
 }
